97f9f30533e99812a2997284b6852878ccc8aac5
Steven Schoovaerts <ragura@gmail.com>
2020-12-10 18:38:35 +0100

    Prototype complete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <img alt="Vue logo" src="./assets/ps5.jpeg" />
+  <h1>Playstation 5 webmonitor</h1>
+  <h2 v-if="connected">Connected to server</h2>
+  <h2 v-else>Not connected to server</h2>
+  <ul v-show="connected">
+    <li v-for="site of sites" :key="site.url">
+      <a :href="site.url">{{ site.title }}</a> :
+      {{ site.changed ? "CHANGED!!!" : "UNCHANGED" }}
+    </li>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import io from "socket.io-client";
+import { ref } from "vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: {},
+  setup() {
+    const socket = io("http://localhost:3000");
+    const connected = ref(false);
+    const sites = ref([]);
+    const alertsound = new Audio("/alert.mp3");
+    // const alarmsound = new Audio("/alarm.mp3");
+
+    socket.on("connect", () => {
+      connected.value = socket.connected;
+    });
+
+    socket.on("disconnect", () => {
+      connected.value = false;
+      // alarmsound.play();
+    });
+
+    socket.on("list", data => {
+      sites.value = data;
+      console.log("Initial list:", data);
+    });
+
+    socket.on("update", data => {
+      if (data.changed) {
+        const index = sites.value.findIndex(site => site.url === data.url);
+        if (index >= 0) {
+          sites.value[index].changed = data.changed;
+        }
+      }
+      alertsound.play();
+    });
+
+    return {
+      connected,
+      sites,
+      alertsound
+    };
   }
 };
 </script>
